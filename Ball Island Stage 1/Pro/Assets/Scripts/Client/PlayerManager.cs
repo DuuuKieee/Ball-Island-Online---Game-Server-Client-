603b867f24d8efb3a5ad19c73a340aca3cba4660
DuuuKieee 1604 <tranthanhloisd@gmail.com>
2023-04-17 11:01:54 +0700

    mapedit

--- Ball Island Stage 1/Pro/Assets/Scripts/Client/PlayerManager.cs
+++ Ball Island Stage 1/Pro/Assets/Scripts/Client/PlayerManager.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerManager : MonoBehaviour
 {
     public int id;
     public string username;
+
 }
