2f14d8f944e393ce14ccc2dbd7917621a3856499
DuuuKieee 1604 <tranthanhloisd@gmail.com>
2023-05-09 16:25:28 +0700

    almost final

---
+++ Ball Island Stage 1/Pro/Assets/Scripts/Client/DatabaseManager.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Authentication;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    MongoClient client = new MongoClient("mongodb+srv://DuuuKieee:899767147@loginserver.hqnkiia.mongodb.net/?retryWrites=true&w=majority");
+    IMongoDatabase database;
+    IMongoCollection<BsonDocument> collection;
+    private UIManager UI;
+    void Start()
+    {
+        database = client.GetDatabase("UserDB");
+        collection = database.GetCollection<BsonDocument>("UserCollection");
+        UI = GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    public async void Register(string Username, string Password)
+    {
+        string user_ = Username;
+        string pass_ = Password;
+        var document = BsonDocument.Parse($"{{ username: \"{user_}\", password: \"{pass_}\" }}");
+
+        await collection.InsertOneAsync(document);
+    }
+
+    public async void Login(string Username, string Password)
+    {
+        string user_ = Username;
+        string pass_ = Password;
+        // Tạo bộ lọc để tìm người dùng có tên đăng nhập tương ứng
+        var filter = Builders<BsonDocument>.Filter.Eq("username", user_);
+
+        var user = await collection.Find(filter).FirstOrDefaultAsync();
+
+    // Kiểm tra xem người dùng có tồn tại hay không
+    if (user != null)
+    {
+        // Lấy mật khẩu đã lưu trữ trong cơ sở dữ liệu
+        var savedPassword = user["password"].AsString;
+        // So sánh mật khẩu được cung cấp bởi người dùng với mật khẩu đã lưu trữ trong cơ sở dữ liệu
+        if (savedPassword.Equals(pass_, StringComparison.OrdinalIgnoreCase))
+
+        {
+                UI.ConnectToServer();
+                Debug.Log("Thanh cong");
+        }
+        else
+        {
+            Debug.Log("Incorrect password");
+        }
+    }
+    else
+    {
+        Debug.Log("User not found");
+    }
+}
+
+}
--- Ball Island Stage 1/Pro/Assets/Scripts/Client/UIManager.cs
+++ Ball Island Stage 1/Pro/Assets/Scripts/Client/UIManager.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
-
     public GameObject startMenu, registerMenu;
-    public InputField usernameField, usernameReg, passwordField, passwordReg;
+    [SerializeField] public InputField usernameField, passwordField, userregField, passwordregField;
+    private DatabaseManager databaseaccess;
+
+    private void Start()
+    {
+        databaseaccess = GameObject.FindGameObjectWithTag("Database").GetComponent<DatabaseManager>();
+
+    }
 
     private void Awake()
     {
@@ -28,12 +37,26 @@ public class UIManager : MonoBehaviour
     {
         startMenu.SetActive(false);
         usernameField.interactable = false;
+        passwordField.interactable = false;
         Client.instance.ConnectToServer();
     }
     public void Register()
     {
+        databaseaccess.Register(userregField.text, passwordregField.text);
+    }
+    public void Login()
+    {
+        databaseaccess.Login(usernameField.text, passwordField.text);
+    }
+
+    public void RegMenu()
+    {
         startMenu.SetActive(false);
         registerMenu.SetActive(true);
-
+    }
+    public void LogMenu()
+    {
+        startMenu.SetActive(true);
+        registerMenu.SetActive(false);
     }
 }
